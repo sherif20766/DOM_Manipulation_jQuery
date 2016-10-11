6d01507079367bb48f68c465675f64284382a79a
Sherif Shawky <sherif20766@hotmail.com>
2016-10-11 20:09:18 +0100

    Created general site layout with functional buttons

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
+
 $(document).ready(function(){
 
+	$('#about').click(function(){
+		$('#option_2').css({'display': 'none'});
+		$('#option_3').css({'display': 'none'});
+		$('#option_1').css({'display': 'block'});
+	});
 
-	
+	$('#menu').click(function(){
+		$('#option_1').css({'display': 'none'});
+		$('#option_3').css({'display': 'none'});
+		$('#option_2').css({'display': 'block'});
+	});
+
+
+	$('#contact').click(function(){
+		$('#option_1').css({'display': 'none'});
+		$('#option_2').css({'display': 'none'});
+		$('#option_3').css({'display': 'block'});
+	});
+
+
 });
